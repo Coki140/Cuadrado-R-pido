db45107cd015d270b75e5057433ebbe3c53563fc
Coki140 <jg2803638@gmail.com>
2024-10-16 11:46:20 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Counter from "../src/components/MIS COMPONENTES/Counter.vue";
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+      />
+      <div class="counters">
+        <Counter title="Entraron" />
+        <Counter title="Salieron" :start="10" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1200px; /* Ajusta según sea necesario */
+  margin: 0 auto; /* Centra el contenedor */
+  display: flex;
+  flex-direction: column; /* Coloca los elementos uno encima del otro en pantallas pequeñas */
+  align-items: center; /* Centra horizontalmente los elementos */
+}
+
+.logo {
+  display: block;
+  margin-bottom: 2rem; /* Espacio entre el logo y los contadores */
+}
+
+.counters {
+  display: flex;
+  justify-content: center; /* Centra los contadores horizontalmente */
+}
+
+header {
+  text-align: center;
+}
+
+@media (min-width: 1000px) {
+  .container {
+    flex-direction: row; /* Cambia a fila en pantallas grandes */
+    justify-content: center; /* Mantiene todo centrado */
+    align-items: center; /* Alinea verticalmente al centro */
+    padding-right: calc(var(--section-gap) / 2);
+    padding-left: calc(
+      var(--section-gap) / 2
+    ); /* Agrega padding a la izquierda para mantenerlo centrado */
+  }
+
+  .logo {
+    margin-right: 2rem; /* Espacio entre el logo y los contadores */
+    margin-bottom: 0; /* Elimina el margen inferior en pantallas grandes */
+  }
+}
+</style>
---
+++ src/components/MIS COMPONENTES/Counter.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+    <button @click="decrease">Disminuir en -1</button>
+    <button @click="increase">Aumentar en +1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 5,
+      validator(value) {
+        return (value) => 5;
+      },
+    },
+  },
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  methods: {
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      this.counter--;
+    },
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter;
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+button {
+  background-color: #64b687;
+  border-radius: 5px;
+  border: 1px solid #fff;
+  color: #fff;
+  cursor: pointer;
+  margin: 10px 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+button:hover {
+  background-color: #2caa62;
+  transition: 0.3s ease-in-out;
+}
+</style>
